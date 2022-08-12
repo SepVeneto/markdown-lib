cf788575b0f8207d9d399b3345a101b14d5fb154
SepVeneto <sepveneto@qq.com>
2022-08-12 17:52:11 +0800

    chore(server): update api

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,6 @@
 import type Router from "koa-router";
 export default function init(router: Router) {
-  router.put('/markdown', async (ctx) => {
+  router.put('/api/v1/markdown', async (ctx) => {
     const files = ctx.request.files
     ctx.response.body = '上传成功'
   })
