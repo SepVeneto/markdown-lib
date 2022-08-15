c08a3459b4025669754026cf14594d11383733be
SepVeneto <sepveneto@qq.com>
2022-08-15 08:57:29 +0800

    chore: nginx容器反向代理

---
+++ scripts/image-nginx.sh
@@ -0,0 +1,1 @@
+docker build --pull --rm -f markdown/dockerfile.nginx -t markdown-nginx:latest "markdown"
---
+++ scripts/image-server.sh
@@ -0,0 +1,1 @@
+docker build --pull --rm -f markdown/dockerfile.server -t markdown-server:latest server
